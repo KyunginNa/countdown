e0a10ccaf133e6473cc63ce367dacf47b878763d
Kyungin Na <nakyungin93@gmail.com>
2024-05-09 19:33:32 +0200

    feat: persist data between page loads

---
+++ src/app/storage.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  setItem(key: string, value: string): void {
+    sessionStorage.setItem(key, value)
+  }
+
+  getItem(key: string): string | null {
+    return sessionStorage.getItem(key)
+  }
+}
